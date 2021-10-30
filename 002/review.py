99ac351590eed2256fc7d6d4f06655e34117ccca
lu1zibrahim <luizibrahim94@gmail.com>
2021-10-30 18:00:52 -0300

    Commit with command line

--- 002/review.py
+++ 002/review.py
@@ -34,3 +34,8 @@ print(variable_name + variable_boolean)
 10 < 5 and 10 <= 25
 print(not(10 == 10 and 10 == "10"))
 10 != 10
+
+# I will test commit with command line
+# git add review.py
+# git commit -m "Commit Message", exit vm if forgot something is :wq
+# git push
