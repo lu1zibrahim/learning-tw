7e5e5c36c7f56daa9ca1dbfe9d34c9241b4c34af
lu1zibrahim <luizibrahim94@gmail.com>
2021-10-31 06:03:34 -0300

    Add the Class review

---
+++ 002/cachorro.py
@@ -0,0 +1,9 @@
+class Cachorro:
+
+    def __init__(self, nome, idade, cor):
+        self.nome = nome
+        self.idade = idade
+        self.cor = cor
+
+    def latir(self):
+        return "Au au"
---
+++ 002/class_review.py
@@ -0,0 +1,11 @@
+from cachorro import Cachorro
+
+cachorro = Cachorro("Beto", 4, "Preto")
+print(cachorro.nome)
+print(cachorro.idade)
+print(cachorro.cor)
+print(cachorro.latir())
+cachorro2 = Cachorro("Tótó", 2, "Caramelo")
+print(cachorro2.nome)
+
+
